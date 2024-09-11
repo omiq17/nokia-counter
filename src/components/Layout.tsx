7e56387cceba905caa8564169bee64b40673fd3a
omiq17 <omiq17@gmail.com>
2024-09-11 09:30:38 +0800

    Add responsive button design.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,9 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className='flex flex-col h-screen'>
       <Header />
-      <main className='flex-1 p-4 flex flex-col items-center justify-center'>{children}</main>
+      <main className='flex-1 p-4 flex flex-col items-center justify-center text-center'>
+        {children}
+      </main>
     </div>
   );
 };
--- src/features/counter/components/Counter.tsx
+++ src/features/counter/components/Counter.tsx
@@ -29,7 +29,7 @@ const Counter: React.FC = () => {
     <div className='flex flex-col items-center justify-center flex-1 p-4'>
       <h1 className='text-2xl font-bold mb-4'>Counter App</h1>
       <p className='text-xl mb-4'>Current Count: {count}</p>
-      <div>
+      <div className='flex flex-col md:flex-row md:justify-between md:space-x-4 space-y-4 md:space-y-0 p-4'>
         <button
           onClick={() => onButtonClick("decrement")}
           className='bg-red-500 text-white px-4 py-2 rounded'
@@ -38,7 +38,7 @@ const Counter: React.FC = () => {
         </button>
         <button
           onClick={() => onButtonClick("increment")}
-          className='bg-blue-500 text-white px-4 py-2 rounded mr-2'
+          className='bg-blue-500 text-white px-4 py-2 rounded'
         >
           Increment
         </button>
